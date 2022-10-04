72d457ca008ba768c641267d5a5182151002b46a
ArahejiElor <aratz.hernandezji@elorrieta-errekamari.com>
2022-10-04 12:29:56 +0200

    SplashScreen Icono

---
+++ app/src/main/java/com/example/nima/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.nima;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.nima.data.LoginDataSource;
+import com.example.nima.data.LoginRepository;
+import com.example.nima.ui.login.LoginActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        TimerTask tarea= new TimerTask(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo=new Timer();
+        tiempo.schedule(tarea,3000);
+
+
+    }
+}

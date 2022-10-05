4f06a12581b363884d711937f7e2532a4c3cd863
boguminillo <julen.sanchezaz@elorrieta-errekamari.com>
2022-10-05 09:41:10 +0200

    Primer intento de login con firebase

--- app/src/main/java/com/example/nima/data/LoginDataSource.java
+++ app/src/main/java/com/example/nima/data/LoginDataSource.java
@@ -1,24 +1,37 @@
 package com.example.nima.data;
 
-import com.example.nima.data.model.LoggedInUser;
+import android.widget.Toast;
 
-import java.io.IOException;
+import com.example.nima.data.model.LoggedInUser;
+import com.example.nima.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
 
+    LoggedInUser user = null;
+
     public Result<LoggedInUser> login(String username, String password) {
         // TODO: handle loggedInUser authentication
-        if (username.equals("nima") && password.equals("123")) {
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        user = new LoggedInUser(Objects.requireNonNull(authResult.getUser()).getUid(), authResult.getUser().getEmail());
+                    }
+                });
+        if (user != null) {
+            return new Result.Success<>(user);
         } else {
-            return new Result.Error(new IOException("Error logging in"));
+            return new Result.Error(new Exception("Error logging in"));
         }
     }
 

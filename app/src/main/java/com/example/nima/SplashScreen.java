19f70c4a3e12788f2a62a4f50f8f63e6dec306ba
julen sanchez <boguminillo@gmail.com>
2022-10-26 08:13:01 +0200

    SplashScreen ahora se pasa pulsando

--- app/src/main/java/com/example/nima/SplashScreen.java
+++ app/src/main/java/com/example/nima/SplashScreen.java
@@ -1,16 +1,14 @@
 package com.example.nima;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import com.example.nima.ui.login.LoginActivity;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashScreen extends AppCompatActivity {
@@ -20,23 +18,17 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        TimerTask tarea = new TimerTask() {
-            @Override
-            public void run() {
-                boolean inicioAutomatico = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getResources().getString(R.string.auto_login), false);
-                Intent intent;
-                if (inicioAutomatico) {
-                    intent = new Intent(SplashScreen.this, MainActivity.class);
-                } else {
-                    intent = new Intent(SplashScreen.this, LoginActivity.class);
-                }
-                startActivity(intent);
-                finish();
+        ImageView imagen = findViewById(R.id.imageView);
+        imagen.setOnClickListener(view -> {
+            boolean inicioAutomatico = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getResources().getString(R.string.auto_login), false);
+            Intent intent;
+            if (inicioAutomatico) {
+                intent = new Intent(SplashScreen.this, MainActivity.class);
+            } else {
+                intent = new Intent(SplashScreen.this, LoginActivity.class);
             }
-        };
-        Timer tiempo = new Timer();
-        tiempo.schedule(tarea, 3000);
-
-
+            startActivity(intent);
+            finish();
+        });
     }
 }

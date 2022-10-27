1d40448da223e208327a7b918b8728df37dc34a4
boguminillo <boguminillo@gmail.com>
2022-10-27 21:35:50 +0200

    Opcion de usar tema por defecto y ajustes en temas para que no haya texto negro en fondos oscuros

--- app/src/main/java/com/example/nima/SplashScreen.java
+++ app/src/main/java/com/example/nima/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.nima;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -17,7 +18,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getResources().getString(R.string.tema_oscuro), false)) {
+        // aplicamos el tema que este guardado en las opciones
+        SharedPreferences opciones = PreferenceManager.getDefaultSharedPreferences(this);
+        if (opciones.getBoolean(getString(R.string.tema_defecto), true)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else if (opciones.getBoolean(getString(R.string.tema_oscuro), true)) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
@@ -26,7 +31,7 @@ public class SplashScreen extends AppCompatActivity {
 
         ImageView imagen = findViewById(R.id.imageView);
         imagen.setOnClickListener(view -> {
-            boolean inicioAutomatico = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(getResources().getString(R.string.auto_login), false);
+            boolean inicioAutomatico = opciones.getBoolean(getString(R.string.auto_login), false);
             Intent intent;
             if (inicioAutomatico) {
                 intent = new Intent(SplashScreen.this, MainActivity.class);
--- app/src/main/java/com/example/nima/ui/SettingsFragment.java
+++ app/src/main/java/com/example/nima/ui/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.example.nima.ui;
 
 import android.os.Bundle;
+
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.example.nima.R;
 
@@ -10,7 +12,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        SwitchPreferenceCompat temaPorDefecto = findPreference(getString(R.string.tema_defecto));
+        SwitchPreferenceCompat temaOscuro = findPreference(getString(R.string.tema_oscuro));
+        assert temaPorDefecto != null;
+        assert temaOscuro != null;
+        // Si el tema por defecto está activado, desactivamos la opcion de tema oscuro
+        if (temaPorDefecto.isChecked())
+            temaOscuro.setEnabled(false);
+        // listener para activar o desactivar la opcion de tema oscuro al cambiar el estado de tema por defecto
+        temaPorDefecto.setOnPreferenceChangeListener((preference, newValue) -> {
+            temaOscuro.setEnabled(!newValue.equals(true));
+            return true;
+        });
     }
-
 
 }
